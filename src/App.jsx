9b67d9d51a2493675d33ccb74da55a6acb659936
Aditya26Das <aditya26.das@gmail.com>
2024-05-14 17:57:42 +0530

    Fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Header/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar/>
     </>
   )
 }
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <Navbar>
+      <Link to="/">Home</Link>
+    </Navbar>
+  )
+}
+
+export default Navbar
